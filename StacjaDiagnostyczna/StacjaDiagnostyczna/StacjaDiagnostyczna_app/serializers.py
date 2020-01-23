771efde82fc01284268f2b4e4f31bc9c6287fe83
Marcin <Marcingalazka79@gmail.com>
2020-01-23 14:33:46 +0100

    work

--- StacjaDiagnostyczna/StacjaDiagnostyczna/StacjaDiagnostyczna_app/serializers.py
+++ StacjaDiagnostyczna/StacjaDiagnostyczna/StacjaDiagnostyczna_app/serializers.py
@@ -3,21 +3,25 @@ from .models import *
 from django.contrib.auth.models import User
 
 class pojazdSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = pojazd
-        fields = ['marka','model','numer_rejestracyjny']
+        fields = ['id','marka','model','numer_rejestracyjny','owner']
 
 class diagnostaSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = diagnosta
-        fields = ['imie','nazwisko','pojazd']
+        fields = ['imie','nazwisko','pojazd','owner']
 
 class klientSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = klient
-        fields = ['imie','nazwisko','telefon','pojazd']
+        fields = ['imie','nazwisko','telefon','pojazd','owner']
 
 class badanieSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = badanie
-        fields = ['pojazd', 'data_badania', 'status', 'ważnosc_badania']
+        fields = ['pojazd', 'data_badania', 'status', 'ważnosc_badania','owner']
--- StacjaDiagnostyczna/StacjaDiagnostyczna/StacjaDiagnostyczna_app/views.py
+++ StacjaDiagnostyczna/StacjaDiagnostyczna/StacjaDiagnostyczna_app/views.py
@@ -21,44 +21,44 @@ from django.http import Http404
 @api_view(['GET','POST'])
 def pojazd_list(request):
     if request.method == 'GET':
-        pojazd = pojazd.objects.all()
-        serializer = pojazdSerializer(pojazd, many=True)
+        pojazd1 = pojazd.objects.all()
+        serializer = pojazdSerializer(pojazd1, many=True)
         #return Response(serializer.data)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     if request.method == 'POST':
         serializer = pojazdSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=self.request.user)
+            serializer.save()
             serializer.save()
             #return Response(serializer.data, status.HTTP_201_CREATED)
-            return JsonResponse(serializer.data, status.HTTP_201_CREATED)
+            return Response(serializer.data, status.HTTP_201_CREATED)
         #return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
-        return JsonResponse(serializer.data, status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def pojazd_detail(request, pk):
     try:
-        pojazd = pojazd.objects.get(pk=pk)
-    except pojazd.DoesNotExist:
+        pojazd1 = pojazd.objects.get(pk=pk)
+    except pojazd1.DoesNotExist:
         return HttpResponse(status=404)
     if request.method == 'GET':
-        serializer = klientSerializer(pojazd)
+        serializer = pojazdSerializer(pojazd1)
         return Response(serializer.data)
     if request.method == 'PUT':
-        serializer = klientSerializer(pojazd, request.data)
+        serializer = klientSerializer(pojazd1, request.data)
         if serializer.is_valid():
             serializer.save();
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
-        pojazd.delete()
+        pojazd1.delete()
         return Response(status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET','POST'])
 def diagnosta_list(request):
     if request.method == 'GET':
-        diagnosta = diagnosta.objects.all()
-        serializer = diagnostaSerializer(diagnosta, many=True)
+        diagnosta1 = diagnosta.objects.all()
+        serializer = diagnostaSerializer(diagnosta1, many=True)
         #return JsonResponse(serializer.data, safe=False)
         return Response(serializer.data)
     if request.method == 'POST':
@@ -73,18 +73,18 @@ def diagnosta_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def diagnosta_detail(request, pk):
         try:
-            diagnosta = diagnosta.objects.get(pk=pk)
+            diagnosta1 = diagnosta.objects.get(pk=pk)
         except diagnosta.DoesNotExist:
             return HttpResponse(status=404)
         if request.method == 'GET':
-            serializer = diagnostaSerializer(diagnosta)
-            return JsonResponse(serializer.data)
+            serializer = diagnostaSerializer(diagnosta1)
+            return Response(serializer.data)
         if request.method == 'PUT':
-            serializer = diagnostaSerializer(diagnosta, request.data)
+            serializer = diagnostaSerializer(diagnosta1, request.data)
             if serializer.is_valid():
                 serializer.save()
-                return JsonResponse(serializer.data, status.HTTP_201_CREATED)
-            return JsonResponse(serializer.data, status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.data, status.HTTP_201_CREATED)
+            return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
         if request.method == 'DELETE':
             diagnosta.delete()
             return Response(status.HTTP_204_NO_CONTENT)
@@ -92,8 +92,8 @@ def diagnosta_detail(request, pk):
 @api_view(['GET','POST'])
 def klient_list(request):
     if request.method == 'GET':
-        klient = klientSerializer.objects.all()
-        serializer = klientSerializer(klient, many=True)
+        klient1 = klient.objects.all()
+        serializer = klientSerializer(klient1, many=True)
         #return JsonResponse(serializer.data, safe=False)
         return Response(serializer.data)
     if request.method == 'POST':
@@ -108,14 +108,14 @@ def klient_list(request):
 @api_view(['GET','PUT','DELETE'])
 def klient_detail(request, pk):
     try:
-        klient = klient.objects.get(pk=pk)
+        klient1 = klient.objects.get(pk=pk)
     except klient.DoesNotExist:
         return HttpResponse(status=404)
     if request.method == 'GET':
-        serializer = klientSerializer(klient)
+        serializer = klientSerializer(klient1)
         return Response(serializer.data)
     if request.method == 'PUT':
-        serializer = klientSerializer(klient, request.data)
+        serializer = klientSerializer(klient1, request.data)
         if serializer.is_valid():
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
@@ -126,8 +126,8 @@ def klient_detail(request, pk):
 @api_view(['GET','POST'])
 def badanie_list(request):
     if request.method == 'GET':
-        badanie = badanieSerializer.objects.all()
-        serializer = badanieSerializer(badanie, many=True)
+        badanie1 = badanie.objects.all()
+        serializer = badanieSerializer(badanie1, many=True)
         #return JsonResponse(serializer.data, safe=False)
         return Response(serializer.data)
     if request.method == 'POST':
@@ -142,18 +142,18 @@ def badanie_list(request):
 @api_view(['GET','PUT','DELETE'])
 def badanie_detail(request, pk):
     try :
-        badanie = badanie.objects.get(pk=pk)
-    except badanie.DoesNotExist:
+        badanie1 = badanie.objects.get(pk=pk)
+    except badanie1.DoesNotExist:
         return HttpResponse(status=404)
     if request.method == 'GET':
-        serializer = badanieSerializer(badanie)
+        serializer = badanieSerializer(badanie1)
         return Response(serializer.data)
     if request.method == 'PUT':
-        serializer = badanieSerializer(badanie, request.data)
+        serializer = badanieSerializer(badanie1, request.data)
         if serializer.is_valid():
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
-        badanie.delete()
+        badanie1.delete()
         return Response(status.HTTP_204_NO_CONTENT)
 
